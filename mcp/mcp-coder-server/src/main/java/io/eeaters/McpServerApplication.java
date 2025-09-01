527b6e627d0bdb8a709bbfa5ecd476637465595d
eeaters <870905780@qq.com>
2025-09-01 15:25:45 +0800

    feat: mcp test

--- mcp/mcp-coder-server/src/main/java/io/eeaters/McpServerApplication.java
+++ mcp/mcp-coder-server/src/main/java/io/eeaters/McpServerApplication.java
@@ -1,14 +1,7 @@
 package io.eeaters;
 
-import org.springaicommunity.mcp.provider.complete.SyncMcpCompleteProvider;
-import org.springaicommunity.mcp.provider.prompt.SyncMcpPromptProvider;
-import org.springaicommunity.mcp.provider.resource.SyncMcpResourceProvider;
-import org.springaicommunity.mcp.provider.tool.SyncMcpToolProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.util.List;
 
 /**
  *
